df6b0bf20329f78a2a3bf09b394fcb7072fc2501
Ubuntu <ubuntu@ip-172-31-39-162.eu-north-1.compute.internal>
2023-12-20 05:53:58 +0000

    c project with makefile

--- big3.c
+++ big3.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 void biggest() {
 
   double n1, n2, n3;
@@ -19,5 +18,6 @@ void biggest() {
   if (n3 >= n1 && n3 >= n2)
     printf("%.2f is the largest number.", n3);
 
- // return 0;
+  //return 0;
 }
+
